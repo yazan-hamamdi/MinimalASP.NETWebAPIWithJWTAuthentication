f056b725c70da5475c05a875aefee955b63263d5
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-11-15 15:25:35 +0200

    Adding JWT generator and Minimal APIs

---
+++ MinimalASP.NETWebAPIWithJWTAuthentication/MinimalASP.NETWebAPIWithJWTAuthentication/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MinimalASP.NETWebAPIWithJWTAuthentication.Services;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddScoped<JwtTokenGenerator>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapPost("/login", (string username, string password, JwtTokenGenerator jwt) =>
+{
+    if (username == "admin" && password == "123")
+    {
+        var token = jwt.GenerateToken(username);
+        return Results.Ok(new { Token = token });
+    }
+
+    return Results.Unauthorized();
+});
+
+app.MapGet("/weather", () =>
+{
+    return new
+    {
+        Date = DateTime.Now.ToShortDateString(),
+        Temperature = "25",
+        Status = "Clear Sky"
+    };
+})
+.RequireAuthorization();
+
+app.MapGet("/welcome", (ClaimsPrincipal user) =>
+{
+    string name = user.Identity?.Name ?? "Unknown";
+    return Results.Ok($"Welcome {name} You are authorized");
+})
+.RequireAuthorization();
+
+app.Run();
---
+++ MinimalASP.NETWebAPIWithJWTAuthentication/MinimalASP.NETWebAPIWithJWTAuthentication/Services/JwtTokenGenerator.cs
@@ -0,0 +1,72 @@
+﻿namespace MinimalASP.NETWebAPIWithJWTAuthentication.Services
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.IdentityModel.Tokens;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+
+    public class JwtTokenGenerator
+    {
+        private readonly IConfiguration _config;
+
+        public JwtTokenGenerator(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken(string username)
+        {
+            var jwtSettings = _config.GetSection("JwtSettings");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpireMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public ClaimsPrincipal? ValidateToken(string token)
+        {
+            try
+            {
+                var jwtSettings = _config.GetSection("JwtSettings");
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+                var parameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.Zero
+                };
+
+                var principal = tokenHandler.ValidateToken(token, parameters, out SecurityToken validatedToken);
+
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
